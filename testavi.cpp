2b78241fd4c41b221eba8c6f0ffe52ef34104194
Khang <khang86175xt@gmail.com>
2025-09-16 17:24:21 +0700

    cài thư viện

--- testavi.cpp
+++ testavi.cpp
@@ -1,23 +1,21 @@
-#include<stdio.h>  
-#include<conio.h>  
-#include<graphics.h>  
-#include<dos.h>  
-main()  
-{     
-    int gd=DETECT,gm;  
-    initgraph(&gd,&gm,(char*)"c:\\tc\\bgi");  
-    setbkcolor(9);  
-    line(130,100,290,100);  
-      
-    line(165,100,165,155);  
-    arc(150,155,100,0,15);  
-    line(180,100,180,170);
-	line(190,100,190,170);  
-  
-    circle(220,140,10);  
-    line(220,130,255,130);  
-    line(255,100,255,170);  
-    line(265,100,265,170);  
+#include <SFML/Graphics.hpp>
 
-    getch();  
+int main() {
+    sf::RenderWindow window(sf::VideoMode({300, 300}), "SFML Window");
+    sf::CircleShape shape(150.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while(window.isOpen()){
+        while(auto event = window.pollEvent()){
+            if(event->is<sf::Event::Closed>()){
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color(64, 64, 64));
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
