5da174b992f0b91dd8cc929dc2d7464637f3625e
Hieu <phambahieu2006@gmail.com>
2025-09-26 00:31:42 +0700

    Them class cannon

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace sf;
 using namespace std;
 
 class Body{
-    private:
+    public:
         Vector2f toaDo;
         Vector2f speed;
         float speedMax, acc;
@@ -34,7 +34,7 @@ class Body{
             toaDo+=speed;
             body.setPosition(toaDo);
         }
-        friend void nhanVatDiChuyen(Body &body);
+        friend void nhanVatDiChuyen(Body &body, RenderWindow &window);
         
 };
 
@@ -46,14 +46,15 @@ class Cannon{
 
         RectangleShape gun;
     public:
-        Cannon(float x, float y) 
-            : dmg(10), range(200), bullet_speed(20), angle(0), toaDo(x, y){
-            gun.setSize({50, 15});
-            gun.setOrigin({10, 7.5});
+        Cannon(Body &body) 
+            : dmg(10), range(200), bullet_speed(20), angle(0), toaDo(body.toaDo){
+            float rong = body.size*0.5, dai = body.size*2;
+            gun.setSize({dai, rong});
+            gun.setOrigin({0, rong/2});
             gun.setFillColor(Color(153,153,153));
             gun.setOutlineThickness(5.f);
             gun.setOutlineColor(Color(102,102,102));
-            gun.setPosition({x,y-25});
+            gun.setPosition(toaDo);
         }
 
         void move(Vector2f pos){
@@ -64,13 +65,14 @@ class Cannon{
         void draw(RenderWindow &window){
             window.draw(gun);
         }
-        void setRotation(float angle){
-            this->angle = angle;
+
+        void setRotation(float newAngle){
+            angle = newAngle;
             gun.setRotation(degrees(angle));
         }
 };
 
-void nhanVatDiChuyen(Body &body){
+void nhanVatDiChuyen(Body &body, RenderWindow &window){
     Vector2f tangToc(0, 0);
     float maSat = 0.9;
     if (Keyboard::isKeyPressed(Keyboard::Key::W)) tangToc.y -= body.acc;
@@ -89,53 +91,49 @@ void nhanVatDiChuyen(Body &body){
     if (abs(body.speed.x) < 0.1) body.speed.x = 0;
     if (abs(body.speed.y) < 0.1) body.speed.y = 0;
     body.move();
+    body.draw(window);
+}
+void updateCannon(Cannon &gun, Body &body, RenderWindow &window){
+    // cập nhật góc quay súng
+    Vector2i mousePos = Mouse::getPosition(window);
+    int dx=mousePos.x - body.toaDo.x;
+    int dy=mousePos.y - body.toaDo.y;
+    int newAngle = atan2(dy,dx)*180/3.14;
+    if(newAngle<0) newAngle+=360;
+
+    gun.setRotation(newAngle);
+    gun.move(body.toaDo);
+    gun.draw(window);
 }
 int main() {
-    int WIDTH = 800; // dài 
-    int HEIGHT = 600; // rộng
+    unsigned int WIDTH = 800, HEIGHT = 600;
     ContextSettings settings;
     settings.attributeFlags=8;
     RenderWindow window(VideoMode({WIDTH, HEIGHT}), "Diep",Style::Default, State::Windowed, settings);
     window.setFramerateLimit(30);
-
-    Body body(WIDTH/2, HEIGHT/2, 75);
-
-     
     
-    int x=WIDTH/2, y=HEIGHT/2;
-    
-
     sf::Font font;
     if(!font.openFromFile("arial.ttf"))
         return -1;
     sf::Text text(font);
     text.setCharacterSize(14);
-    text.setFillColor(sf::Color::Red);
+    text.setFillColor(Color::Red);
+
+    Body body(WIDTH/2, HEIGHT/2, 75);
+    Cannon gun(body);
 
 
     while(window.isOpen()){
         while(auto event = window.pollEvent()){
-            if(event->is<sf::Event::Closed>()){
+            if(event->is<Event::Closed>()){
                 window.close();
             }
         }
-        nhanVatDiChuyen(body);
-        
-        
+        window.clear(Color(204, 204, 204));
 
-        sf::Vector2i mousePos = sf::Mouse::getPosition(window);
-        int dx=mousePos.x-x, dy=mousePos.y-y;
-        int angle=atan2(dy,dx)*180/3.14;
-        std::stringstream ss;
-        ss << mousePos.x <<" "<<mousePos.y<<" "<<angle;
-        gun.setPosition({x+cos((angle-90)*3.14/180)*25,y+sin((angle-90)*3.14/180)*25});
-        text.setString(ss.str());
-        gun.setRotation(sf::degrees(angle));
-        window.clear(sf::Color(204, 204, 204));
-        window.draw(text);
-
-        body.draw(window);
-
+        updateCannon(gun, body, window);
+        nhanVatDiChuyen(body, window);
+                   
         window.display();
     }
 
